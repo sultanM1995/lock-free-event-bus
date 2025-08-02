427ec1775d9d8bbd3370117f81cf8fc71ac7c73d
sultanM1995 <mahmud.seeum@gmail.com>
2025-08-02 18:01:51 +0100

    implement ticketing approach and per slot sequence for mpsc

--- includes/lock_free_mpsc_queue.hpp
+++ includes/lock_free_mpsc_queue.hpp
@@ -10,40 +10,58 @@ namespace eventbus {
 
     public:
         explicit LockFreeMpscQueue(const size_t capacity)
-               : capacity_(capacity + 1),  // +1 to distinguish full from empty
-                 buffer_(std::make_unique<T[]>(capacity_)),
+               : capacity_(capacity),
+                 buffer_(std::make_unique<node_[]>(capacity_)),
                  head_(0),
-                 tail_(0)
-        {}
+                 tail_(0) {
+            for (size_t i = 0; i < capacity_; ++i) {
+                buffer_[i].seq_.store(i, std::memory_order_relaxed);
+            }
+        }
 
         bool enqueue(const T& item) {
-            const size_t current_tail = tail_.fetch_add(1, std::memory_order_acq_rel);
-            const size_t idx = current_tail % capacity_;
-            const size_t next_idx = (current_tail + 1) % capacity_;
-            const size_t current_head = head_.load(std::memory_order_acquire);
-            if (next_idx == current_head) {
-                tail_.fetch_sub(1, std::memory_order_acq_rel);
+            const size_t pos = tail_.fetch_add(1, std::memory_order_acq_rel); // take a ticket
+            size_t slot_index = pos & (capacity_ - 1); // find the physical slot index
+            node_& node = buffer_[slot_index];
+
+            size_t node_seq = node.seq_.load(std::memory_order_acquire); // take the slot sequence
+
+            if (node_seq != pos) {
                 return false;
             }
-            buffer_[idx] = item;
+
+            node.item_ = item;
+            node.seq_.store(pos+1, std::memory_order_release);
             return true;
         }
 
         bool dequeue(T& item) {
-            const size_t current_head = head_.load(std::memory_order_relaxed);
-            const size_t current_tail = tail_.load(std::memory_order_acquire);
-            if (current_tail % capacity_ == current_head) {
+            const size_t pos = head_.load(std::memory_order_relaxed);
+            size_t slot_index = pos & (capacity_ - 1);
+
+            node_& node = buffer_[slot_index];
+
+            size_t node_seq = node.seq_.load(std::memory_order_acquire);
+
+            if (node_seq != pos + 1) {
                 return false;
             }
-            item = buffer_[current_head];
-            head_.store((current_head + 1) % capacity_, std::memory_order_release);
+
+            item = node.item_;
+            node.seq_.store(pos + capacity_, std::memory_order_release);
+
+            head_.store(pos + 1, std::memory_order_relaxed);
             return true;
         }
 
 
     private:
+        struct node_ {
+            T item_;
+            std::atomic<size_t> seq_;
+        };
         size_t capacity_;
-        std::unique_ptr<T[]> buffer_;
+        std::unique_ptr<node_[]> buffer_;
         alignas(64) atomic<size_t> head_;
         alignas(64) atomic<size_t> tail_;
     };
